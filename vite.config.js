220790a3252cf8bfb49214c13280e129ee076bc4
Nafi Hermawan <nafihermawan@gmail.com>
2025-07-01 20:23:51 +0700

    Add new icons and update Vite configuration for PWA support

--- vite.config.js
+++ vite.config.js
@@ -17,19 +17,18 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/',
         icons: [
-          {
-            src: '/icon.png',
-            sizes: '842x814',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
-          }
-        ]
+  {
+    src: '/icon1.png',
+    sizes: '192x192',
+    type: 'image/png'
+  },
+  {
+    src: '/icon2.png',
+    sizes: '512x512',
+    type: 'image/png',
+    purpose: 'any maskable'
+  }
+]
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
